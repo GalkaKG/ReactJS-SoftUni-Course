8e2c62438eb34309cda1c3cb365e9502b5b748ee
ggeorgieva <galina.georgieva@oblak.bg>
2024-01-18 17:00:52 +0200

    added more toggle functions

--- components-deep-dive-demo/src/components/Book.js
+++ components-deep-dive-demo/src/components/Book.js
@@ -2,27 +2,43 @@ import { useEffect, useState } from "react";
 
 export const Book = (props) => {
     const [highlited, setHighlited] = useState(false);
+    const [marked, setMarked] = useState(false);
 
     useEffect(() => {
-        console.log('Mounting');
+        console.log('Mounting: ' + props.title);
+        debugger;
     }, [])
+
+    useEffect(() => {
+        console.log('Updating: ' + props.title);
+    }, [highlited, marked])
 
     const clickHandler = () => {
         setHighlited(state => !state);
     };
+
+    const deleteHandler = () => {
+        setMarked(true);
+    }
 
     let style = {};
     if (highlited) {
         style.backgroundColor = 'blue';
     }
 
+    if(marked) {
+        style.backgroundColor = 'red';
+    }
+
     return (
-        <li onClick={clickHandler} style={style}>
+        <li style={style}>
             <article>
                 <h2>{props.title}</h2>
                 <div>Year: {props.year}</div>
                 <div>{props.edition}</div>
                 <footer>
+                    <button onClick={clickHandler}>Highlight</button>
+                    <button onClick={deleteHandler}>Delete</button>
                     <span>Author: {props.author}</span>
                 </footer>
             </article>
