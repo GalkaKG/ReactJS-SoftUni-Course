ecfdd63f16253fa0cd99fead65cbb27596656f79
GalkaKG <galina_georgieva_net@abv.bg>
2024-01-21 20:26:00 +0200

    added components with header

--- users-list-workshop/client/src/App.js
+++ users-list-workshop/client/src/App.js
@@ -1,7 +1,9 @@
+import { Header } from "./components/Header";
+
 function App() {
   return (
     <div className="App">
-      
+        < Header />
     </div>
   );
 }
---
+++ users-list-workshop/client/src/components/Header.js
@@ -0,0 +1,9 @@
+export const Header = () => {
+    return (
+        <header class="header">
+            <div class="logo">
+            <span class="course">React Workshop - September 2023</span>
+            </div>
+        </header>
+    )
+}
