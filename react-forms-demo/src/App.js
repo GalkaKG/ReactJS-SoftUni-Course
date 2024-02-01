9b5dcaa8bb40c7ebb7be05d84cc25bfb84330f37
ggeorgieva <galina.georgieva@oblak.bg>
2024-02-01 12:00:27 +0200

    added form

--- react-forms-demo/src/App.js
+++ react-forms-demo/src/App.js
@@ -2,21 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    
+    const formData = new FormData(e.currentTarget);
+    const username = formData.get('username');
+    const password = formData.get('password');
+    console.log(username);
+    console.log(password);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={submitHandler}>
+            <div>
+              <label htmlFor="username">Username</label>
+              <input id="username" type="text" name="username" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input id="password" type="text" name="password"/>
+            </div>
+            <div>
+              <input type="submit" value="Login" />
+              {/* <button type="submit">Login</button> */}
+            </div>
+        </form>
+        {/* <button type="button">Click</button> */}
       </header>
     </div>
   );
