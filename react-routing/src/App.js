b19de6a684d921d88c3bd422294510db56886db7
GalkaKG <galina_georgieva_net@abv.bg>
2024-02-20 19:21:43 +0200

    added routes

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React Router</h1>
+
+      <Routes>
+        <Route path='/' element={<h2>Home page</h2>} />
+        <Route path='/about' element={<h2>About page</h2>} />
+        <Route path='/pricing' element={<h2>Pricing page</h2>} />
+        <Route path='/contacts' element={<h2>Contacts page</h2>} />
+      </Routes>
     </div>
   );
 }
