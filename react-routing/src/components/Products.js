adfd046c2410f14a3c0233e021c3d96cbec33089
GalkaKG <galina_georgieva_net@abv.bg>
2024-02-28 19:19:51 +0200

    fetched the swapi

--- react-routing/src/components/Products.js
+++ react-routing/src/components/Products.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function Products() {
+    const [starship, setStarship] = useState({});
     // const params = useParams();
     const { productId } = useParams();
+
+    useEffect(() => {
+        fetch(`https://swapi.dev/api/starships/${productId}/`)
+        .then(res => res.json())
+        .then(result => {
+            setStarship(result);
+        })
+    });
+
+    const nextProductHandler = () => {
+        // TODO: Redirect to next product
+    }
 
     return (
         <>
@@ -14,6 +28,14 @@ export default function Products() {
                 {/* Product {params.productId} Specification */}
                 Product {productId} Specification
             </h3>
+
+            <ul>
+                <li>Name: {starship.name}</li>
+                <li>Model: {starship.model}</li>
+                <li>Manufacturer: {starship.manufacturer}</li>
+            </ul>
+
+            <button onClick={nextProductHandler}>Next</button>
         </>
     )
 }
