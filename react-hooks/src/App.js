12e2dd07529651d65ce67bb3e678038c6396cf22
Galina Georgieva <ggeorgieva@haemimont.com>
2024-03-05 11:20:04 +0200

    added styles

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react';
+import TaskList from './components/TaskList';
+import styles from './App.module.css';
 
 
 function App() {
   
   return (
-    <div className='site-wrapper'>
+    <div className={styles['site-wrapper']}>
       <header>TODO App</header>
 
       <main>
-
+          <TaskList />
       </main>
     </div>
   );
